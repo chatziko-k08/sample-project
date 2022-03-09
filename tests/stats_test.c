ca0402f3945e98d55184ace8eb3c648a5f2d9ed2
Kostas Chatzikokolakis <kostas@chatzi.org>
2022-03-09 14:45:40 +0200

    stats_test: TEST_CHECK -> TEST_ASSERT

--- tests/stats_test.c
+++ tests/stats_test.c
@@ -13,21 +13,21 @@
 void test_find_min(void) {
 	int array[] = { 3, 1, -1, 50 };
 
-	TEST_CHECK(stats_find_min(array, 4) == -1);
-	TEST_CHECK(stats_find_min(array, 3) == -1);
-	TEST_CHECK(stats_find_min(array, 2) == 1);
-	TEST_CHECK(stats_find_min(array, 1) == 3);
-	TEST_CHECK(stats_find_min(array, 0) == INT_MAX);
+	TEST_ASSERT(stats_find_min(array, 4) == -1);
+	TEST_ASSERT(stats_find_min(array, 3) == -1);
+	TEST_ASSERT(stats_find_min(array, 2) == 1);
+	TEST_ASSERT(stats_find_min(array, 1) == 3);
+	TEST_ASSERT(stats_find_min(array, 0) == INT_MAX);
 }
 
 void test_find_max(void) {
 	int array[] = { 3, 1, -1, 50 };
 
-	TEST_CHECK(stats_find_max(array, 4) == 50);
-	TEST_CHECK(stats_find_max(array, 3) == 3);
-	TEST_CHECK(stats_find_max(array, 2) == 3);
-	TEST_CHECK(stats_find_max(array, 1) == 3);
-	TEST_CHECK(stats_find_max(array, 0) == INT_MIN);
+	TEST_ASSERT(stats_find_max(array, 4) == 50);
+	TEST_ASSERT(stats_find_max(array, 3) == 3);
+	TEST_ASSERT(stats_find_max(array, 2) == 3);
+	TEST_ASSERT(stats_find_max(array, 1) == 3);
+	TEST_ASSERT(stats_find_max(array, 0) == INT_MIN);
 }
 
 // Λίστα με όλα τα tests προς εκτέλεση
